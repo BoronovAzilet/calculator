1c89a8143ddcddaad4ae7b2874cbe40bde92571f
Azilet <Azilet>
2023-03-16 17:09:12 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var result = document.getElementById("result");
+
+function addToResult(value) {
+  result.value += value;
+}
+
+function clearResult() {
+  result.value = "";
+}
+
+function backspace() {
+  result.value = result.value.slice(0, -1);
+}
+
+function calculate(operator) {
+  var expression = result.value;
+  if (operator === "=") {
+    result.value = eval(expression);
+  } else {
+    result.value += operator;
+  }
+}
